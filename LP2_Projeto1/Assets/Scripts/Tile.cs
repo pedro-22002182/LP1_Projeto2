ceeb234edeb6d9aa3d32a8177f97e1c8a197ecbf
Pectron <pedropassos.dev@gmail.com>
2021-11-24 11:20:57 +0000

    Rename class field to tile

--- LP2_Projeto1/Assets/Scripts/Tile.cs
+++ LP2_Projeto1/Assets/Scripts/Tile.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Field : MonoBehaviour
+public class Tile : MonoBehaviour
 {
     private ICollection<IResource> resources;
     private int baseGoldValue;
