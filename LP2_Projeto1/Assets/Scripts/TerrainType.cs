5e9aece165cda9efc27a84dcc7a82a511cfd7311
peta <coisasdohapo@gmail.com>
2021-11-24 11:13:11 +0000

    Creat terrain type enum

---
+++ LP2_Projeto1/Assets/Scripts/TerrainType.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainType : MonoBehaviour
+{
+    public enum terrainType
+    {
+        Desert,
+        Grassland,
+        Hills,
+        Mountain,
+        Ocean
+    }
+}
