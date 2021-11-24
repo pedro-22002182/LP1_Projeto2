6304dfc24d4ec94c4691c72157774f59662dc06b
Pectron <pedropassos.dev@gmail.com>
2021-11-24 10:56:57 +0000

    Add Script Resource and Enum ResourceType

--- LP2_Projeto1/Assets/Scripts/Resource.cs
+++ LP2_Projeto1/Assets/Scripts/Resource.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Resource : MonoBehaviour
+public class Resource : IResource
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private ResourceType type;
 }
---
+++ LP2_Projeto1/Assets/Scripts/ResourceType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ResourceType
+{
+    Plants,
+    Animals,
+    Metals,
+    Fossil_Fuel,
+    Luxury,
+    Pollution,
+}
