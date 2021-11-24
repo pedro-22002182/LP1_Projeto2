3dda637c8441e16c3e792cb1fab6ecf5e39ff54a
Jose Reis <josereis.dev@gmail.com>
2021-11-24 12:34:26 +0000

    Add constructor and define values

--- LP2_Projeto1/Assets/Scripts/Tile.cs
+++ LP2_Projeto1/Assets/Scripts/Tile.cs
@@ -8,6 +8,72 @@ public class Tile : MonoBehaviour
     private int baseFoodValue;
 
     public TerrainType Terrain { get; }
-    public int GoldProduced { get; }
-    public int FoodProduced { get; }
+    
+    public int GoldProduced
+    {
+        get
+        {
+            int goldProduction = baseGoldValue;
+
+            foreach (IResource r in resources)
+            {
+                goldProduction =+ r.GoldValue;
+            }
+
+            return goldProduction;
+        }
+    }
+
+    public int FoodProduced
+    {
+        get
+        {
+           int foodProduction = baseFoodValue;
+
+            foreach (IResource r in resources)
+            {
+                foodProduction =+ r.FoodValue;
+            }
+
+            return foodProduction; 
+        }
+    }
+
+    public Tile(TerrainType terrain, ICollection<IResource> resources)
+    {
+        Terrain = terrain;
+        this.resources = resources;
+        DefineBaseValues();
+    }
+
+    private void DefineBaseValues()
+    {
+        switch (Terrain)
+        {
+            case TerrainType.Desert:
+                baseGoldValue = 0;
+                baseFoodValue = 0;
+                break;
+            
+            case TerrainType.Grassland:
+                baseGoldValue = 0;
+                baseFoodValue = 2;
+                break;
+            
+            case TerrainType.Hills:
+                baseGoldValue = 1;
+                baseFoodValue = 1;
+                break;
+            
+            case TerrainType.Mountain:
+                baseGoldValue = 1;
+                baseFoodValue = 0;
+                break;
+
+            case TerrainType.Ocean:
+                baseGoldValue = 0;
+                baseFoodValue = 1;
+                break;
+        }
+    }
 }
