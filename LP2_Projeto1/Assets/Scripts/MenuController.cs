a924621f8417ff3d4f87567c0778a4c8df59d413
peta <coisasdohapo@gmail.com>
2021-11-26 17:17:59 +0000

    Add start menu and tweak file opener script

---
+++ LP2_Projeto1/Assets/Scripts/MenuController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
