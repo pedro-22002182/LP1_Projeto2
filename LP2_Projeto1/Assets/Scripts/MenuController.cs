cae7269c2c811f864dedc2181f81c5367fea41a4
peta <coisasdohapo@gmail.com>
2021-11-26 23:00:19 +0000

    Create 5 future buttons, select new map file button

--- LP2_Projeto1/Assets/Scripts/MenuController.cs
+++ LP2_Projeto1/Assets/Scripts/MenuController.cs
@@ -1,11 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject ButtonsContainer;
+    private bool IsMenu;
     public void CloseGame()
     {
         Application.Quit();
     }
+
+    public void OpenAnotherMapFile()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+    void update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("+ressed");
+            IsMenu = !IsMenu;
+            if(IsMenu)
+            {
+                ButtonsContainer.SetActive(true);
+            }
+            else
+            {
+                ButtonsContainer.SetActive(false);
+            }
+        }
+    }
+    
 }
