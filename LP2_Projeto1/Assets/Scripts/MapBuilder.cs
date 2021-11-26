65e09b2dd95c6196589aec907d20c2b95dd2a12c
peta <coisasdohapo@gmail.com>
2021-11-26 20:38:52 +0000

    Fix merge conflicts

--- LP2_Projeto1/Assets/Scripts/MapBuilder.cs
+++ LP2_Projeto1/Assets/Scripts/MapBuilder.cs
@@ -49,7 +49,7 @@ public class MapBuilder : MonoBehaviour
             */
 
             BuildMap();
-            SceneManager.LoadScene("jogo");
+            SceneManager.LoadScene("Jogo");
 		}
     }
 
