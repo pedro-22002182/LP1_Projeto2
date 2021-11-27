5ed5c33b1bf11e1328190a3144c1dd5da40f5094
Jose Reis <josereis.dev@gmail.com>
2021-11-27 17:37:42 +0000

    Add instructions scene

--- LP2_Projeto1/Assets/Scripts/TestScripts/Controller.cs
+++ LP2_Projeto1/Assets/Scripts/TestScripts/Controller.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Controller : MonoBehaviour
 {
@@ -13,5 +14,15 @@ public class Controller : MonoBehaviour
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
+    }
+
+    public void LoadInstructionsScene()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void LoadMainMenuScene()
+    {
+        SceneManager.LoadScene("TestMainMenu");
     }
 }
